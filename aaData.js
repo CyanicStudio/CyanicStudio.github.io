6440727e1bf6077cd3300d1374b8a05c40bec5f4
Cody Nhan Pham <nhanstar111@gmail.com>
2021-10-10 22:30:03 -0500

    Change img folder name and fix aaData.js

--- aaData.js
+++ aaData.js
@@ -1,121 +1,121 @@
 aaInfo = [ 
     {
         "name": "Alanine",
-        "image": "./.media/Alanine.png",
+        "image": "./media/Alanine.png",
         "oneLetter": "A",
         "threeLetter": "Ala"
     },
     {
         "name": "Arginine",
-        "image": "./.media/Arginine.png",
+        "image": "./media/Arginine.png",
         "oneLetter": "R",
         "threeLetter": "Arg"
     },
     {
         "name": "Asparagine",
-        "image": "./.media/Asparagine.png",
+        "image": "./media/Asparagine.png",
         "oneLetter": "N",
         "threeLetter": "Asn"
     },
     {
         "name": "Aspartic acid",
-        "image": "./.media/Aspartic-acid.png",
+        "image": "./media/Aspartic-acid.png",
         "oneLetter": "D",
         "threeLetter": "Asp"
     },
     {
         "name": "Cysteine",
-        "image": "./.media/Cysteine.png",
+        "image": "./media/Cysteine.png",
         "oneLetter": "C",
         "threeLetter": "Cys"
     },
     {
         "name": "Glutamic acid",
-        "image": "./.media/Glutamic-acid.png",
+        "image": "./media/Glutamic-acid.png",
         "oneLetter": "E",
         "threeLetter": "Glu"
     },
     {
         "name": "Glutamine",
-        "image": "./.media/Glutamine.png",
+        "image": "./media/Glutamine.png",
         "oneLetter": "Q",
         "threeLetter": "Gln"
     },
     {
         "name": "Glycine",
-        "image": "./.media/Glycine.png",
+        "image": "./media/Glycine.png",
         "oneLetter": "G",
         "threeLetter": "Gly"
     },
     {
         "name": "Histidine",
-        "image": "./.media/Histidine.png",
+        "image": "./media/Histidine.png",
         "oneLetter": "H",
         "threeLetter": "His"
     },
     {
         "name": "Isoleucine",
-        "image": "./.media/Isoleucine.png",
+        "image": "./media/Isoleucine.png",
         "oneLetter": "I",
         "threeLetter": "Ile"
     },
     {
         "name": "Leucine",
-        "image": "./.media/Leucine.png",
+        "image": "./media/Leucine.png",
         "oneLetter": "L",
         "threeLetter": "Leu"
     },
     {
         "name": "Lysine",
-        "image": "./.media/Lysine.png",
+        "image": "./media/Lysine.png",
         "oneLetter": "K",
         "threeLetter": "Lys"
     },
     {
         "name": "Methionine",
-        "image": "./.media/Methionine.png",
+        "image": "./media/Methionine.png",
         "oneLetter": "M",
         "threeLetter": "Met"
     },
     {
         "name": "Phenylalanine",
-        "image": "./.media/Phenylalanine.png",
+        "image": "./media/Phenylalanine.png",
         "oneLetter": "F",
         "threeLetter": "Phe"
     },
     {
         "name": "Proline",
-        "image": "./.media/Proline.png",
+        "image": "./media/Proline.png",
         "oneLetter": "P",
         "threeLetter": "Pro"
     },
     {
         "name": "Serine",
-        "image": "./.media/Serine.png",
+        "image": "./media/Serine.png",
         "oneLetter": "S",
         "threeLetter": "Ser"
     },
     {
         "name": "Threonine",
-        "image": "./.media/Threonine.png",
+        "image": "./media/Threonine.png",
         "oneLetter": "T",
         "threeLetter": "Thr"
     },
     {
         "name": "Tryptophan",
-        "image": "./.media/Tryptophan.png",
+        "image": "./media/Tryptophan.png",
         "oneLetter": "W",
         "threeLetter": "Trp"
     },
     {
         "name": "Tyrosine",
-        "image": "./.media/Tyrosine.png",
+        "image": "./media/Tyrosine.png",
         "oneLetter": "Y",
         "threeLetter": "Tyr"
     },
     {
         "name": "Valine",
-        "image": "./.media/Valine.png",
+        "image": "./media/Valine.png",
         "oneLetter": "V",
         "threeLetter": "Val"
     }
